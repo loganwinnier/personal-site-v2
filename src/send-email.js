43f042eb43cca58b01deef0910adacb350590cac
Logan Winnie <loganwinnier@gmail.com>
2023-12-29 11:55:40 -0500

    added SSR

---
+++ src/send-email.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.zoho.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+    },
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+        from: process.env.EMAIL, // sender address
+        to: 'loganwinnier@gmail.com', // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+}
+
+button = Document.querySelector('#send-mail');
+button.addEventListener('click', main
+    .catch(console.error))
+
